bb7ca52815ebcf740f46319760f9a9d32430268b
Turhut Bilalov <turhutbilalov@gmail.com>
2025-08-17 21:43:49 +0200

    Update time-of-day.sql

--- outputs/time-of-day.sql
+++ outputs/time-of-day.sql
@@ -1,1 +1,17 @@
-
+with cte as (
+    select *,
+           case 
+               when hour(timestamp) between 6 and 11 then 'Morning'
+               when hour(timestamp) between 12 and 17 then 'Afternoon'
+               when hour(timestamp) between 18 and 23 then 'Evening'
+               else 'Night'
+           end as Time_of_Day
+    from logistics_data
+)
+select Time_of_Day,
+       count(*) AS Total_Shipments,
+       sum( logistics_Delay) AS Delayed_Shipments,
+       round( sum(logistics_Delay) / count(*) * 100,1) AS Delay_Percentage
+from cte
+group by Time_of_Day
+order by 4 desc;
